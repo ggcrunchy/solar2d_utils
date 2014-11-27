2cf17d171fd27442f16ca979970b979e25c7bdec
Steven Johnson <steve@xibalbastudios.com>
2014-11-26 18:58:17 -0600

    Some restructuring and de-hard-coding of the audio module, more probably to follow soon (plus finally implementing some things); audio.NewSoundGroup_Multi() to accommodate multiple prefices

--- audio.lua
+++ audio.lua
@@ -30,6 +30,9 @@ local pairs = pairs
 local random = math.random
 local type = type
 
+-- Modules --
+local strings = require("tektite_core.var.strings")
+
 -- Corona globals --
 local system = system
 local timer = timer
@@ -40,17 +43,35 @@ local audio = require("audio")
 -- Exports --
 local M = {}
 
--- Groups of related audio, e.g. all sounds related to a type of object --
-local Groups = {}
+--
+local function AddToGroup (info, sounds)
+	--
+	local base, prefix = sounds._base, sounds._prefix or ""
 
--- Plays a bit of audio (unless it has already played too recently)
-local function Play (group, handles, name, delay)
-	if delay then
-		return timer.performWithDelay(delay, function()
-			Play(group, handles, name)
-		end)
+	if #prefix == 0 then
+		prefix = "SFX/"
+	elseif not strings.EndsWith(prefix, "/") then
+		prefix = prefix .. "/"
 	end
 
+	--
+	for k, sinfo in pairs(sounds) do
+		if k ~= "_base" and k ~= "_prefix" then
+			local itype = type(sinfo)
+
+			if itype == "string" then
+				info[k] = { m_file = prefix .. sinfo, m_base = base }
+			else
+				assert(itype == "table", "Non-table sound info")
+
+				info[k] = { m_file = prefix .. sinfo.file, m_base = base, m_wait = sinfo.wait }
+			end
+		end
+	end
+end
+
+-- Common play logic
+local function AuxPlay (group, handles, name)
 	local handle = handles[name]
 
 	if handle --[[and SoundOn]] then
@@ -70,9 +91,64 @@ local function Play (group, handles, name, delay)
 	end
 end
 
--- Helper to resolve sound effect filenames
-local function File (name)
-	return "SFX/" .. name
+-- Plays a bit of audio (unless it has already played too recently)
+local function Play (group, handles, name, delay)
+	local thandle
+
+	if delay then
+		thandle = timer.performWithDelay(delay, function()
+			AuxPlay(group, handles, name)
+		end)
+	else
+		AuxPlay(group, handles, name)
+	end
+
+	return thandle
+end
+
+-- Groups of related audio, e.g. all sounds related to a type of object --
+local Groups = {}
+
+-- Common logic for making a sound group
+local function AuxNewSoundGroup (info)
+	-- Streamline the sounds list into a group.
+	local SoundGroup = { m_info = info }
+
+	--- Initializes a group. This must be called before the group is used to play any sounds.
+	--
+	-- After loading, this will be a no-op until the level is unloaded.
+	function SoundGroup:Load ()
+		if not self.m_handles then
+			local handles = {}
+
+			for k, sinfo in pairs(self.m_info) do
+				handles[k] = audio.loadSound(sinfo.m_file, sinfo.m_base)
+			end
+
+			self.m_handles = handles
+		end
+	end
+
+	--- Utility.
+	-- @param name Name of sound to play.
+	-- @uint[opt] delay Optional delay, in milliseconds, before playing.
+	-- @treturn TimerHandle A timer that may be cancelled, or **nil** if _delay_ was absent.
+	function SoundGroup:PlaySound (name, delay)
+		return Play(self, assert(self.m_handles, "Sound group not loaded"), name, delay)
+	end
+
+	--- If the group has an array part, plays one of its sounds.
+	-- @uint[opt] delay Optional delay, in milliseconds, before playing.
+	-- @treturn TimerHandle A timer that may be cancelled, or **nil** if _delay_ was absent.
+	function SoundGroup:RandomSound (delay)
+		local handles = assert(self.m_handles, "Sound group not loaded")
+
+		return Play(self, handles, random(#handles), delay)
+	end
+
+	Groups[#Groups + 1] = SoundGroup
+
+	return SoundGroup
 end
 
 --- Builds a new group of sounds. A group is lazy: unless loaded, it only contains some
@@ -90,58 +166,22 @@ end
 -- it is ignored.
 -- @treturn table Sound group object.
 function M.NewSoundGroup (sounds)
-	-- Streamline the sounds list into a group.
 	local info = {}
 
-	for k, sinfo in pairs(sounds) do
-		local itype = type(sinfo)
+	AddToGroup(info, sounds)
 
-		if itype == "string" then
-			info[k] = { m_file = File(sinfo) }
-		else
-			assert(itype == "table", "Non-table sound info")
+	return AuxNewSoundGroup(info)
+end
 
-			info[k] = { m_file = File(sinfo.file), m_wait = sinfo.wait }
-		end
+--- DOCME
+function M.NewSoundGroup_Multi (arr)
+	local info = {}
+
+	for _, sounds in ipairs(arr) do
+		AddToGroup(info, sounds)
 	end
 
-	local group = { m_info = info }
-
-	--- Initializes a group. This must be called before the group is used to play any sounds.
-	--
-	-- After loading, this will be a no-op until the level is unloaded.
-	function group:Load ()
-		if not self.m_handles then
-			local handles = {}
-
-			for k, sinfo in pairs(self.m_info) do
-				handles[k] = audio.loadSound(sinfo.m_file)
-			end
-
-			self.m_handles = handles
-		end
-	end
-
-	--- Utility.
-	-- @param name Name of sound to play.
-	-- @uint[opt] delay Optional delay, in milliseconds, before playing.
-	-- @treturn TimerHandle A timer that may be cancelled, or **nil** if _delay_ was absent.
-	function group:PlaySound (name, delay)
-		return Play(self, assert(self.m_handles, "Sound group not loaded"), name, delay)
-	end
-
-	--- If the group has an array part, plays one of its sounds.
-	-- @uint[opt] delay Optional delay, in milliseconds, before playing.
-	-- @treturn TimerHandle A timer that may be cancelled, or **nil** if _delay_ was absent.
-	function group:RandomSound (delay)
-		local handles = assert(self.m_handles, "Sound group not loaded")
-
-		return Play(self, handles, random(#handles), delay)
-	end
-
-	Groups[#Groups + 1] = group
-
-	return group
+	return AuxNewSoundGroup(info)
 end
 
 -- TODO: Enable / disable audio
