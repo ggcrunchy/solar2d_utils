e765cf992516151d9462f8db9452feb797b278ed
Steven Johnson <steve@xibalbastudios.com>
2017-03-17 19:01:14 -0600

    Some formatting fixes

--- device.lua
+++ device.lua
@@ -66,10 +66,10 @@ local function IdentifyGamepad (device)
 		Gamepads[device] = "PS3"
 	elseif name:find(XInput) then
 		Gamepads[device] = "Xbox360"
-  elseif device.MFiProfile then
-    local mfip = device.MFiProfile
+	elseif device.MFiProfile then
+		local mfip = device.MFiProfile
 
-    Gamepads[device] = "MFi" .. mfip:sub(1, 1):upper() .. mfip:sub(2)
+		Gamepads[device] = "MFi" .. mfip:sub(1, 1):upper() .. mfip:sub(2)
 	else -- TODO: several others, also test different platforms
 		Gamepads[device] = "unknown"
 	end
@@ -246,113 +246,6 @@ if false then
 	end
 
 	Runtime:addEventListener("key", KeyEvent)
-end
-
---[[
-	InputDevice:
-
-Properties
-
-object.androidDeviceId (temporary)
-object.canVibrate
-object.connectionState - "connected", "disconnected" (reserved: "-ing" versions)
-object.descriptor
-object.displayName
-object.isConnected
-object.permanentId (preferred, if available)
-object.type (one of the following)
-
-"keyboard"
-"mouse"
-"stylus"
-"trackball"
-"touchpad"
-"touchscreen"
-"joystick"
-"gamepad"
-"directionalPad"
-"unknown"
-
-
-
-Methods
-
-object:getAxes() - {} of InputAxis
-object:vibrate()
-]]
-
---[[
-	InputAxis:
-
-object.accuracy (margin of error)
-object.descriptor
-object.maxValue
-object.minValue
-object.number
-object.type ("unreliable")
-
-min/maxValue:
-
-A joystick typically has a maximum and minimum value range of 1.0 and -1.0 respectively.
-
-A touchscreen's maximum value will usually be the length of the screen in pixels, with a minimum value of zero.
-
-A touchpad's maximum value is typically a large value using an unknown unit of measure, meaning you have to evaluate the relative position of the received axis
-data based on the min and max value properties.
-
-number:
-
-The number assigned to an input device's axis. This number is based on the number of axes found on one input device. For example, if an input device has 4 axes,
-then they will be assigned numbers 1, 2, 3, and 4 in the order that they were found. You can use this number to uniquely identify an axis belonging to one input device.
-
-This axis number is the only reliable means of identifying an axis belonging to one input device. This is because it is possible for the axis type property to return
-duplicate type names, which is especially true if the types are "unknown". Also, because the type names do not always match what was actually pressed on the device, which
-is especially true for gamepads and joysticks designed for another platform in mind. If you need to display the axis' name to the end-user, then you should display the axis
-number instead of the type name.
-
-This axis number can be used as an index with the array returned by its input device's getAxes() function.
-
-]]
-
-function M.Show ()
-    local text = display.newText("WAITING", 0, 0, native.systemFontBold, 35)
-
-    local T = {}--[[
-    local function print (a, b, c)
-        T[#T+1]=tostring(a) .. "  " .. tostring(b or "") .. "  " .. tostring(c or "")
-    end]]
-    
-    local function PrintDevice (device, i)
-        print( i, "canVibrate", device.canVibrate )
-		print( i, "connectionState", device.connectionState )
-		print( i, "descriptor", device.descriptor )
-		print( i, "displayName", device.displayName )
-		print( i, "isConnected", device.isConnected )
-		print( i, "type", device.type )
-		print( i, "permenantid", tostring(device.permanentId) )
---		print( i, "androidDeviceid", device.androidDeviceId )
-	end
-	
-	for i, device in ipairs(system.getInputDevices()) do
-	    PrintDevice(device, i)
-	end
-	
-    local function InputDeviceStatusChanged (event)
-	   print(0, "STATUS CHANGE!")
-	   PrintDevice(event.device, 0)
-	end
-
-	Runtime:addEventListener("inputDeviceStatus", InputDeviceStatusChanged)
-
-    timer.performWithDelay(2500, function()
-        if #T == 0 then
-            text.text = "NO TEXT"
-        else
-            text.text = table.remove(T, 1)
-        end
-        text.x = display.contentCenterX
-        text.y = display.contentHeight - 50
-    end, 0)
 end
 
 -- Adapted from ponywolf's joykey:
