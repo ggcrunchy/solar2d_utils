eddea1c2e8dbbe9e9823259f30136c680b260690
Steven Johnson <steve@xibalbastudios.com>
2019-11-06 19:07:29 -0600

    collision module broken in half, now have visibility as well (name of this and its functions might change)

--- collision.lua
+++ collision.lua
@@ -30,6 +30,7 @@ local pairs = pairs
 -- Modules --
 local args = require("iterator_ops.args")
 local powers_of_2 = require("bitwise_ops.powers_of_2")
+local visibility = require("corona_utils.visibility")
 
 -- Corona globals --
 local display = display
@@ -40,7 +41,7 @@ local timer = timer
 local physics = require("physics")
 
 -- Cached module references --
-local _SetVisibility_
+local _GetType_
 
 -- Exports --
 local M = {}
@@ -59,17 +60,15 @@ function M.Activate (object, active)
 	end)
 end
 
--- Collision handlers --
 local Handlers = {}
 
 --- Defines a collision handler for objects of a given collision type with other objects.
 --
 -- When two objects collide, each is checked for a handler. Each handler that exists is
 -- called as
---    handler(phase, object, other, other_type, contact)
+--    handler(phase, object, other, contact)
 -- where _phase_ is **"began"** or **"ended"**, _object_ is what supplied _handler_, _other_
--- is what collided with _object_, _other_type_ is the collision type of _other_ (may be
--- **nil**), and _contact_ is the physics object passed via the event.
+-- is what collided with _object_, and _contact_ is the physics object passed via the event.
 --
 -- The collision type of _object_ is not supplied, being implicit in the handler itself. For
 -- all practical purposes, _type_ is just a convenient name for _func_; there is no real
@@ -80,117 +79,8 @@ function M.AddHandler (type, func)
 	Handlers[type] = func
 end
 
-local Watching
-
---- DOCME
-function M.Dirty (object)
-	if display.isValid(object) then
-		local watch = Watching[object] or {}
-
-		for other in pairs(watch) do
-			watch[other] = 0
-		end
-
-		Watching[object] = watch
-	end
-end
-
--- --
-local IsHidden, Partners
-
--- Types used to manage physics interactions --
-local Types = {}
-
---- DOCME
-function M.DoIfVisible (object, other, func)
-	local object_valid, other_valid = display.isValid(object), display.isValid(other)
-
-	if object_valid and other_valid and object ~= other and not (IsHidden[object] or IsHidden[other]) then
-		func(object, other, Types[other], true)
-	end
-end
-
---
-local function WipeState (event)
-	local object = event.target
-
-	if IsHidden then
-		IsHidden[object], Partners[object] = nil
-	end
-
-	if Watching then
-		Watching[object] = nil
-	end
-
-	Types[object] = nil
-end
-
---
-local function Check (object)
-	if IsHidden[object] == nil then
-		object:addEventListener("finalize", WipeState)
-
-		IsHidden[object] = false
-	end
-end
-
---
-local function AddToList (object, other, func)
-	Check(object)
-
-	local list = Partners[object] or {}
-
-	Partners[object], list[other] = list, func
-end
-
---
-local function InList (object, other)
-	local list = Partners[object]
-
-	return list and list[other]
-end
-
---
-local function RemoveFromList (object, other)
-	local list = Partners[object]
-
-	if list then
-		list[other] = nil
-	end
-end
-
---- DOCME
-function M.DoOrDefer (object, other, phase, func)
-	if object ~= other and display.isValid(object) and display.isValid(other) then
-		-- Phase "began", objects intact: if at least one of the objects is hidden, defer the
-		-- action; otherwise, perform it immediately.
-		if phase == "began" then
-			--
-			AddToList(object, other, func)
-
-			if not InList(other, object) then
-				AddToList(other, object, false)
-			end
-
-			--
-			if not (IsHidden[object] or IsHidden[other]) then
-				IsHidden[object] = "immediate"
-
-				_SetVisibility_(object, true)
-			end
-
-		-- Phase "ended", objects intact: break pairings.
-		else
-			RemoveFromList(object, other)
-			RemoveFromList(other, object)
-		end
-	end
-end
-
--- Next mask to allocate --
 local Mask = 0x1
 
--- Lazily named filter flags --
 local NamedFlags = setmetatable({}, {
 	__index = function(t, k)
 		assert(Mask < 0xFFFF, "Limit of 16 named flags")
@@ -223,20 +113,14 @@ function M.FilterBits (...)
 	return bits
 end
 
+local Types = {}
+
 ---
 -- @param object Object to query.
 -- @return Collision type of _object_, or **nil** if absent.
 -- @see SetType
 function M.GetType (object)
-	return Types[object]
-end
-
----
--- @pobject object Object to poll about visibility.
--- @treturn boolean Is _object_ visible?
--- @see SetVisibility
-function M.IsVisible (object)
-	return not IsHidden[object]
+	return display.isValid(object) and Types[object] or nil -- fallthrough if untyped
 end
 
 --- Assign a sensor body to an object.
@@ -253,7 +137,7 @@ end
 -- @param object Object with body to remove.
 -- @treturn boolean Was a body removed?
 function M.RemoveBody (object)
-	return Types[object] ~= nil and physics.removeBody(object)
+	return _GetType_(object) ~= nil and physics.removeBody(object)
 end
 
 --- Associate a collision type with _object_. This is used to choose _object_'s handler in
@@ -263,96 +147,34 @@ end
 -- @param type Type to assign, or **nil** to untype _object_.
 -- @see AddHandler, GetType
 function M.SetType (object, type)
-	Check(object)
-
 	Types[object] = type
 end
 
---- DOCME
-function M.SetVisibility (object, show)
-	--
-	if display.isValid(object) then
-		local hide, is_hidden = not show, IsHidden[object]
-
-		--
-		IsHidden[object] = hide
-
-		--
-		if hide then
-			Check(object)
-
-		--
-		elseif is_hidden then
-			local list1 = Partners[object]
-
-			if list1 then
-				local otype, is_immediate = Types[object], is_hidden == "immediate"
-
-				for other, func in pairs(list1) do
-					local intact = display.isValid(other)
-
-					if not (intact and IsHidden[other]) then
-						if intact then
-							if func then
-								func(object, other, Types[other], is_immediate)
-							end
-
-							--
-							local list2 = Partners[other]
-							local func2 = list2 and list2[object]
-
-							if func2 then
-								func2(other, object, otype, is_immediate)
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-end
-
-local function EnterLevel ()
-	if not IsHidden then
+local function EnterLevel (event)
+	if event.name == "enter_level" then
 		physics.start()
 		physics.setGravity(0, 0)
 	end
 
-	IsHidden, Partners, Watching = {}, {}, {}
-end
-
---
-local function Watch (o1, o2, value)
-	local w1, w2 = Watching[o1], Watching[o2]
-
-	if w1 or w2 then
-		local v1, v2
-
-		if w1 then
-			w1[o2], v1 = value, w1[o2]
-		end
-
-		if w2 then
-			w2[o1], v2 = value, w2[o1]
-		end
-			
-		return v1 or v2
-	end
+	visibility.Start()
 end
 
 local FrameID
 
 local function AuxCollision (phase, o1, o2, contact)
-	local t1, t2 = Types[o1], Types[o2]
-	local h1, h2 = Handlers[t1], Handlers[t2]
+	local h1, h2 = Handlers[_GetType_(o1)], Handlers[_GetType_(o2)]
 
 	if h1 then
-		h1(phase, o1, o2, t2, contact)
+		h1(phase, o1, o2, contact)
 	end
 
 	if h2 then
-		h2(phase, o2, o1, t1, contact)
+		h2(phase, o2, o1, contact)
 	end
+end
+
+local function OnEnded (object, other)
+	AuxCollision("ended", object, other, false)
 end
 
 for k, v in pairs{
@@ -360,37 +182,14 @@ for k, v in pairs{
 	collision = function(event)
 		local o1, o2, phase = event.object1, event.object2, event.phase
 
-		if phase ~= "began" then
-			Watch(o1, o2, nil)
-		elseif Watch(o1, o2, FrameID) then
-			return
-		end
+		visibility.OnCollision(o1, o2, phase, FrameID)
 
 		AuxCollision(phase, o1, o2, event.contact)
 	end,
 
 	-- enterFrame --
 	enterFrame = function(event)
-		if Watching then
-			-- Check all (valid) pairs being watched. If any objects are still colliding
-			-- (since they collided again when a new body was added), keep the entries
-			-- around but ignore the objects again until one of them is dirtied. If the
-			-- entry has gone stale, break the connection.
-			for object, w1 in pairs(Watching) do
-				for other, frame in pairs(w1) do
-					if display.isValid(other) then
-						if frame == FrameID then
-							Watch(object, other, false)
-						elseif frame then
-							Watch(object, other, nil)
-							AuxCollision("ended", object, other, false)
-						end
-					else
-						w1[other] = nil
-					end
-				end
-			end
-
+		if visibility.Update(FrameID, OnEnded) then
 			FrameID = event.frame
 		end
 	end,
@@ -401,8 +200,7 @@ for k, v in pairs{
 	-- Leave Level --
 	leave_level = function()
 		physics.stop()
-
-		IsHidden, Partners, Watching = nil
+		visibility.Stop()
 	end,
 
 	-- Reset Level --
@@ -411,6 +209,6 @@ for k, v in pairs{
 	Runtime:addEventListener(k, v)
 end
 
-_SetVisibility_ = M.SetVisibility
+_GetType_ = M.GetType
 
 return M
